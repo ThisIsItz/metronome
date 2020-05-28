6b6d1abd42855e4b4ec26fac7020e7f40e7ec6be
ThisIsItz <itziarurbieta94@gmail.com>
2020-05-28 16:52:01 +0100

    Created component and added style

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './Metronome.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+    render(){
+        let bpm = 100;
+        let playing = false;
+
+        return(
+            <div className="metronome">
+                <div className="bpm-slider">
+                    <div>{bpm} BPM</div>
+                    <input type="range" min="20" max="400" value={bpm}/>
+                </div>
+                <button className="button">{playing ? "Stop" : "Start"}</button>
+            </div>
+        )
+    }
 }
 
-export default App;
+export default App
