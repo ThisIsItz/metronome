0cff1b507da8129d4fae39d9ca0a33b7cac087fb
ThisIsItz <itziarurbieta94@gmail.com>
2020-05-29 11:18:24 +0100

    Added change to bpm

--- src/App.js
+++ src/App.js
@@ -2,15 +2,30 @@ import React, { Component } from 'react'
 import './Metronome.css'
 
 class App extends Component{
+    constructor(props){
+        super(props);
+
+        this.state = {
+            playing: false,
+            bpm: 100,
+            count: 0,
+            beatsPerMeasure: 4,
+        }
+    }
+
+    changeBpm = event => {
+        const bpm = event.target.value;
+        this.setState({bpm})
+    }
+
     render(){
-        let bpm = 100;
-        let playing = false;
+        const { playing, bpm } = this.state;
 
         return(
             <div className="metronome">
                 <div className="bpm-slider">
                     <div>{bpm} BPM</div>
-                    <input type="range" min="20" max="400" value={bpm}/>
+                    <input type="range" min="20" max="400" value={bpm} onChange={this.changeBpm}/>
                 </div>
                 <button className="button">{playing ? "Stop" : "Start"}</button>
             </div>
