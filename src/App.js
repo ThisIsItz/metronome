af5ecd6e561fe095c6edc25464dedf8506f91962
ThisIsItz <itziarurbieta94@gmail.com>
2020-06-02 11:52:29 +0100

    Added sound per beats

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import './Metronome.css'
-import './click1.wav'
-import './click2.wav'
+import audio1 from './click1.wav'
+import audio2 from './click2.wav'
 
 class App extends Component{
     constructor(props){
@@ -13,7 +13,44 @@ class App extends Component{
             count: 0,
             beatsPerMeasure: 4,
         }
+
+        this.lowClick = new Audio(audio1)
+        this.highClick = new Audio(audio2)
     }
+
+    startAudio = () => {
+        if (this.state.playing){
+            clearInterval(this.timer)
+            this.setState({
+                playing: false
+            })
+        }else{
+            this.timer = setInterval(
+                this.playClick,
+                    (60/ this.state.bpm) * 1000
+            )
+            this.setState({
+                playing: true,
+                count: 0
+            },
+                this.playClick  
+            )
+        }
+    }
+
+    playClick = () => {
+        const { count, beatsPerMeasure } = this.state;
+        if(count % beatsPerMeasure === 0){
+            this.highClick.play()
+        }else{
+            this.lowClick.play()
+        }
+
+        this.setState(state => ({
+            count: (state.count + 1) % state.beatsPerMeasure
+        }))
+    }
+
 
     changeBpm = event => {
         const bpm = event.target.value;
@@ -29,7 +66,7 @@ class App extends Component{
                     <div>{bpm} BPM</div>
                     <input type="range" min="20" max="400" value={bpm} onChange={this.changeBpm}/>
                 </div>
-                <button className="button">{playing ? "Stop" : "Start"}</button>
+                <button className="button" onClick={this.startAudio}>{playing ? "Stop" : "Start"}</button>
             </div>
         )
     }
